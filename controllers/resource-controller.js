15536b4fe9f05f5c9f732071a310020604089cef
lrbn86 <lrbn86@gmail.com>
2024-05-25 16:34:02 -0400

    Create all CRUD operations

--- controllers/resource-controller.js
+++ controllers/resource-controller.js
@@ -1,6 +1,55 @@
+const createResource = (req, res) => {
+  return res.json({
+    message: 'Creating resource'
+  });
+};
+
 const getResource = (req, res) => {
+  return res.json({
+    message: 'GET ALL Resource'
+  });
+};
+
+const getResourceById = (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!id) {
+    return res.status(400).json({
+      message: 'Id parameter required'
+    });
+  }
+  return res.json({
+    message: `GET Resource ${id}`,
+  });
+};
+
+const updateResourceById = (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!id) {
+    return res.status(400).json({
+      message: 'Id parameter required'
+    });
+  }
+  return res.json({
+    message: `Updating Resource ${id}`
+  });
+};
+
+const deleteResourceById = (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!id) {
+    return res.status(400).json({
+      message: 'Id parameter required'
+    });
+  }
+  return res.json({
+    message: `Deleting Resource ${id}`
+  });
 };
 
 module.exports = {
-  getResource
+  createResource,
+  getResource,
+  getResourceById,
+  updateResourceById,
+  deleteResourceById
 };
